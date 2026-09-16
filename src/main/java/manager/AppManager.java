8e781e4a9fd9ee3eb51940b07196c25b9b6080ec
zakrroma <zakrroma@gmail.com>
2026-09-16 20:32:33 +0300

    Phonebook

--- src/main/java/manager/AppManager.java
+++ src/main/java/manager/AppManager.java
@@ -2,6 +2,9 @@ package manager;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.Browser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.annotations.AfterMethod;
@@ -17,10 +20,19 @@ public class AppManager {
     }
 
     public Logger logger = LoggerFactory.getLogger(AppManager.class);
+    static String browser = System.getProperty("browser",
+            Browser.CHROME.browserName());
 
     @BeforeMethod(alwaysRun = true)
     public void setUp(Method method) {
-        driver = new ChromeDriver();
+        if (browser.equals(Browser.CHROME.browserName())) {
+            driver = new ChromeDriver();
+        } else if (browser.equals(Browser.FIREFOX.browserName())) {
+            driver = new FirefoxDriver();
+        } else if (browser.equals(Browser.EDGE.browserName())) {
+            driver = new EdgeDriver();
+        }
+
         driver.manage().window().maximize();
         logger.info("Test started with method: " + method.getName());
     }
